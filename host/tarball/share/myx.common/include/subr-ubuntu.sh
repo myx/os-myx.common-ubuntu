da070683a46263b83d079fd5f4f4d87fa711854a
Alexander Kharichev <myx@myx.co.nz>
2018-05-20 00:02:54 +0300

    * updated

---
+++ host/tarball/share/myx.common/include/subr-ubuntu.sh
@@ -0,0 +1,6 @@
+#
+# Not executable as a separate unit.
+#
+
+. "/usr/local/share/myx.common/include/subr-universal.sh"
+
---
+++ sh-scripts/install-myx.common-ubuntu.sh
@@ -0,0 +1,36 @@
+#!/bin/sh -e
+
+# There are two ways:
+#
+# 1) fetch https://raw.githubusercontent.com/myx/os-myx.common-ubuntu/master/sh-scripts/install-myx.common-ubuntu.sh -o - | sh -e
+# or
+# 2) To execute this as a script, run:
+#		sh -c 'eval "`cat`"'
+# on the target machine under the 'root' user, paste whole text from this file, then press CTRL+D.
+#
+
+echo "myx.common FreeBSD Installer started..." >&2
+
+#
+# Check user
+#
+test `id -u` != 0 && echo 'ERROR: Must be root!' && exit 1
+
+fetch https://github.com/myx/os-myx.common/archive/master.zip -o - | \
+		tar zxvf - --cd "/usr/local/" --include "*/host/tarball/*" --strip-components 3
+		
+fetch https://github.com/myx/os-myx.common-ubuntu/archive/master.zip -o - | \
+		tar zxvf - --cd "/usr/local/" --include "*/host/tarball/*" --strip-components 3
+
+chown root:wheel "/usr/local/bin/myx.common"
+chmod 755 "/usr/local/bin/myx.common"
+
+chown -R root:wheel "/usr/local/share/myx.common/bin"
+chmod -R 750 "/usr/local/share/myx.common/bin"
+
+# exec "/usr/local/share/myx.common/bin/reinstall"
+
+#
+# completion for root in bash:
+# 		myx.common setup/console
+#
