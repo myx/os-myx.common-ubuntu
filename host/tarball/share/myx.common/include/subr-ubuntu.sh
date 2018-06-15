f5494a2600be8e7ed3d7672d7b090adf58fe28b7
Alexander Kharichev <myx@myx.co.nz>
2018-06-15 22:23:58 +0300

    + implementations of generic abstract functions

--- host/tarball/share/myx.common/include/subr-ubuntu.sh
+++ host/tarball/share/myx.common/include/subr-ubuntu.sh
@@ -4,3 +4,16 @@
 
 . "/usr/local/share/myx.common/include/subr-universal.sh"
 
+
+UmanWheelGroupName(){
+	echo "adm"
+}
+
+UmanBaseHomeDirectory(){
+	echo "/home"
+}
+
+UmanRootHomeDirectory(){
+	echo "/root"
+}
+
---
+++ host/tarball/share/myx.common/include/subr.sh
@@ -0,0 +1,5 @@
+#
+# Not executable as a separate unit.
+#
+
+. "/usr/local/share/myx.common/include/subr-ubuntu.sh"
